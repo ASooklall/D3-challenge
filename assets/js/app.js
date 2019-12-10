de77f87cd51cecd0fc018f41afad524ddaae2deb
Andrew Sooklall <andrewsooklall@gmail.com>
2019-12-10 12:56:51 -0500

    created functions for dynamic updating of chart

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,8 +2,9 @@
 //////////////////////////////// Assignment 16 - D3 Challenge ////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////
 
-
-// SVG //
+//////////////////////////////////////////
+///////////// Initialize SVG /////////////
+//////////////////////////////////////////
 
 // set parameters for SVG
 var svgWidth = 960;
@@ -29,3 +30,77 @@ var svg = d3
 // append an SVG group
 var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+
+//////////////////////////////////////////
+///////// Functions for Elements /////////
+//////////////////////////////////////////
+
+// set initial param
+var chosenXAxis = "in_poverty";
+
+// function used for updating x-scale var upon click on axis label
+function xScale(data, chosenXAxis) {
+    // create scales
+    var xLinearScale = d3.scaleLinear()
+      .domain([d3.min(data, d => d[chosenXAxis]) * 0.8,
+        d3.max(data, d => d[chosenXAxis]) * 1.2
+      ])
+      .range([0, width]);
+  
+    return xLinearScale;
+  
+  }
+  
+  // update xAxis var when label is clicked
+  function renderAxes(newXScale, xAxis) {
+    var bottomAxis = d3.axisBottom(newXScale);
+  
+    xAxis.transition()
+      .duration(1000)
+      .call(bottomAxis);
+  
+    return xAxis;
+  }
+  
+  // update circles group with transitions
+  function renderCircles(circlesGroup, newXScale, chosenXAxis) {
+  
+    circlesGroup.transition()
+      .duration(1000)
+      .attr("cx", d => newXScale(d[chosenXAxis]));
+  
+    return circlesGroup;
+  }
+  
+  // update circles group with new tooltip
+  function updateToolTip(chosenXAxis, circlesGroup) {
+  
+    if (chosenXAxis === "in_poverty") {
+      var label = "In Poverty (%):";
+    }
+    else {
+      var label = "Household Income (Median)";
+    }
+  
+    var toolTip = d3.tip()
+      .attr("class", "tooltip")
+      .offset([80, -60])
+      .html(d => {
+        return (`Test <hr>${d.poverty}<hr> ${d[chosenXAxis]}`);
+      });
+  
+    circlesGroup.call(toolTip);
+
+      // mouseover event - show tooltip
+    circlesGroup.on("mouseover", function(data) {
+      toolTip.show(data);
+    })
+      // onmouseout event - hide tooltip
+      .on("mouseout", function(data, index) {
+        toolTip.hide(data);
+      });
+  
+    return circlesGroup;
+  }
+
