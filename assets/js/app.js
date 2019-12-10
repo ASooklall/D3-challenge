9706d53b90f79755e68633b5e8d7e03145304c4a
Andrew Sooklall <andrewsooklall@gmail.com>
2019-12-10 12:42:06 -0500

    initialized svg wrappers for d3

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,31 @@
-// @TODO: YOUR CODE HERE!
+//////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////// Assignment 16 - D3 Challenge ////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// SVG //
+
+// set parameters for SVG
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 80,
+  left: 100
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// create SVG wrapper and append a group to hold the chart 
+var svg = d3
+  .select(".chart")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+// append an SVG group
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
